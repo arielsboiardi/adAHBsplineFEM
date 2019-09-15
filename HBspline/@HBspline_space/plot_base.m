8517359c0f1ceca510ee0a089a62925735ce5037
arielsboiardi <ariels.boiardi@gmail.com>
2019-09-15 21:56:13 +0200

    Update plot_base.m
    Migliorato algoritmo selezione dei colori per aumentare la legiilità

--- HBspline/@HBspline_space/plot_base.m
+++ HBspline/@HBspline_space/plot_base.m
@@ -7,21 +7,30 @@ function plot_base(hspace, varargin)
 % INPUTS:
 %
 %   hspace:     spazio di cui tracciare la base
-%   varargin:   aromnti opzionali della funzione plot per risultati grafici
-%               particolari
+%   varargin:   argomenti opzionali della funzione plot per risultati 
+%               grafici particolari
 %
 % OUTPUT:
 %
 %   figura con i grafici delle funzioni della base gerarchica
 %
+% NOTE:
+%   1. I colori dei livelli sono generati casualemnte per creare in modo
+%   semplice un numero arbitario di combinazioni crmatiche in scala RGB.
+%   L'impostazione del seed della funzione random per ogni livello consente
+%   però di ottenere lo stesso risultato per ogni esecuzione. Pe il
+%   generatore ho scelto Combined Multiple Recursive perché genera una
+%   successione a mio parere più piacevole enaturale. 
 
 t=hspace.knots(1):0.001:hspace.knots(end);
 
-figure
+
+% figure
 hold on
 P{1}=[];
 for L=1:hspace.nlev
     spaceL=hspace.sp_lev{L};
+    rng(L,'combRecursive');
     clrL=rand(1,3);
     leg_info{L}=strcat("Livello", num2str(L));
     for jdx=find(hspace.A{L})
