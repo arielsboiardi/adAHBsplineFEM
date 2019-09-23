2dced957b3fe1a63b135a6888065cb4860f78b5f
arielsboiardi <ariels.boiardi@gmail.com>
2019-09-23 23:05:04 +0200

    Create HBspline_solver_set.m
    Impostazioni del risolutora adattivo in B-spline gerarchiche

---
+++ HBspline/HBspline_FEM/HBspline_solver_set.m
@@ -0,0 +1,18 @@
+classdef HBspline_solver_set
+    
+    properties
+        maxDoF          % Numero di gradi di libertà a cui arrestare la procedura.
+        maxIter         % Numero di iterazioni a cui arrestare la procedura.
+        minPercImpr     % Minimo valore del miglioramento possibile rispetto alla stima globale dell'errore per cui ha senso iterare il procediento.
+        minIterImpr     % Minimo valore del miglioramento in una iterazione per cui si stima abbia senso prosegure.
+        maxRes          % Massimo valore accettabile per lo stimatore residuale dell'errore globale.
+        maxResLoc       % Massimo valore accettabile per lo stimatore residuale dell'errore locale.
+        Marker          % Selezione del marcatore.
+        theta           % Valore del parametro theta del marcatore selezionato.
+        PreMark         % Attivazione del premarker.
+        PreMarkPerc     % Percetuale di premarking
+        
+        VerboseMode     % Attivazione della modalità verbosa che commenta i risultati intermedi nella finestra dei comandi. 
+    end
+    
+end
