b087619e1487ba73f43f3298afb4b82153f0ee24
arielsboiardi <ariels.boiardi@gmail.com>
2019-09-24 09:09:00 +0200

    Update HBspline_solver_out.m
    Aggiunta descrizione

--- HBspline/HBspline_FEM/HBspline_solver_out.m
+++ HBspline/HBspline_FEM/HBspline_solver_out.m
@@ -1,4 +1,13 @@
 classdef HBspline_solver_out
+    % HBspline_solver_out contiene le informazioni di uscita del risolutore
+    % adattivo in spazi di B-splines gerarchiche. 
+    %   Una volta ottenuta la soluzione prodotta dal risolutore adattivo,
+    %   l'utente potrebbe voler esaminare ache alcuni risultati intermedi
+    %   ed essere in grado di valutare la qualità della soluzione. Un
+    %   oggetto di questa classe, prodotto come output ad ogni esecuzione
+    %   di HBspline_adaptive_solver contiene le informazioni che riteniamo
+    %   utili a questo scopo. 
+    
     properties
         NoIter          % Numero di iterazioni in cui è stata raggiunta la soluzione.
         NoDoF           % Numero di gradi di libertà della soluzione raggiunta.
