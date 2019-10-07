162a7d232232443f56bc81e0abf004148bc12a72
arielsboiardi <ariels.boiardi@gmail.com>
2019-10-07 10:55:08 +0200

    Update get_supported_functions.m
    Corrette descrizioni

--- Bspline/@Bspline_space/get_supported_functions.m
+++ Bspline/@Bspline_space/get_supported_functions.m
@@ -1,6 +1,6 @@
 function supp_funs=get_supported_functions(space, fun_ind)
-% get_supported_functions calcola le funzioni della base di space che hanno
-% supporto comune con le funzioni indicizzate in fun_ind.
+% get_supported_functions calcola gli indici delle b-aplines della base di 
+% space che hanno supporto comune con le funzioni indicizzate in fun_ind.
 %
 %   supp_funs=get_supported_functions(space,fun_ind)
 %
@@ -14,12 +14,13 @@ function supp_funs=get_supported_functions(space, fun_ind)
 % OUTPUTS:
 %
 %   supp_funs:  indici delle B-spline con supporto in comune con le B-pline
-%               indicate da fun_ind.
+%               indicate da fun_ind. Gli indici sono nella stessa forma di
+%               quelli di input. 
 %
 % NOTE:
 %   
-%   1. In questo caso siccome l'operazione non può essere parallelizzata ma
-%   necessita di essere svolta una funzione per volta, uso gli indici
+%   1. In questo caso siccome l'operazione non può essere vettorializzata,
+%   ma necessita di essere svolta una funzione per volta, uso gli indici
 %   sequenziali. Nel caso vengano forniti indici logici li converto, in
 %   modo da aumentare l'interoperabilità con altri codici. Restituisco
 %   indici dello stesso tipo di quello dati in input per lo stesso motivo. 
@@ -39,7 +40,7 @@ for fundx=fun_ind
     knots=space.knots(knots_ind); % leggo i nodi toccati da fundx
     knots_ind=find(knots_ind);
     
-    v=[1:knots_ind(1)-1];   % seleziona i nodi antecedenti ala funzione 
+    v=[1:knots_ind(1)-1];   % seleziona i nodi antecedenti alla funzione 
     pm=-1;   % questo parametro esprime se si seleziona in avanti o indietro
     
     for check=knots([1,end])
