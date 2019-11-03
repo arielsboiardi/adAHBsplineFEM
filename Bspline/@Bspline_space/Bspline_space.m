34459836cbe28ec8ef88877f6419199d56526ce7
arielsboiardi <ariels.boiardi@gmail.com>
2019-11-03 18:56:01 +0100

    Translation of comments and descriptions

--- Bspline/@Bspline_space/Bspline_space.m
+++ Bspline/@Bspline_space/Bspline_space.m
@@ -1,27 +1,34 @@
 classdef Bspline_space
-    % Bspline_space contiene le informazioni necessarie per costruire uno
-    % spazio di B-spline.
+    % Bspline_space contains information necessary to represent a B-spline 
+    % space, as illustrated in the report. 
     
     properties
-        deg         % grado delle B-splines
-        knots       % vettore di nodi 
-        dim         % dimensione dello spazio
+        deg         % B-splines degree
+        knots       % Knot vector of the B-splines
+        dim         % Dimension of the spanned space
     end
     
     methods
         
         function space=Bspline_space(deg, knots)
-            % Costruisce un oggetto Bspline_space con gli attributi
-            % compatibili con i dati. Il vettore di nodi knots deve essere
-            % non esteso.
+            % Constructor of the Bspline_space class
+            %
+            %   space=Bspline_space(deg, knots)
+            %
+            % INPUTS:
+            %  
+            %   deg:    degree of the B-splines
+            %   knots:  non-extended knot vector
+            %
+            % OUTPUTS:
+            %
+            %   space:  instance of the class
+            %
+            
             space.deg=deg;
-            reshape(knots,1,numel(knots)); % metto in riga
-            space.knots=[knots(1)*ones(1,deg),knots,knots(end)*ones(1,deg)]; % estendo il vettore di nodi
-            % Dalla teoria sappiamo che la dimensione dello spazio di
-            % B-splines è pari alla somma delle molteplicità dei nodi
-            % interni non ripetuti M + deg + 1, pertanto, poiché la
-            % partizione estesa contiene M+2+2p nodi,
-            space.dim=numel(space.knots)-deg-1;
+            reshape(knots,1,numel(knots)); % transform in row 
+            space.knots=[knots(1)*ones(1,deg),knots,knots(end)*ones(1,deg)]; % extend knot vector with padding accordin to deg
+            space.dim=numel(space.knots)-deg-1; % As computed in the report
         end 
 
     end
