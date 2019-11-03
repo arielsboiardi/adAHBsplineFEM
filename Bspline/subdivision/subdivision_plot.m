170974e0dd8b0d94ec48f2688c6ad45b035d3510
arielsboiardi <ariels.boiardi@gmail.com>
2019-11-03 18:46:24 +0100

    Update subdivision_plot.m

--- Bspline/subdivision/subdivision_plot.m
+++ Bspline/subdivision/subdivision_plot.m
@@ -28,9 +28,10 @@ for ind=f_ind
     plot(t,space.Bspline_eval(ind,t));
     
     for kdx=1:p+2
-        if 2*ind+kdx-(p+2) > 0
+        jdx=2*ind+kdx-(p+2)
+        if jdx > 0
             b=@(x) 2^(-p) * nchoosek(p+1,kdx-1) * ...
-                spacer.Bspline_eval(2*ind+kdx-(p+2), x);
+                spacer.Bspline_eval(jdx, x);
             plot(t,b(t))
         end
     end
