aee360c3b62f3bd372257c7fe9caad4c704d2efe
arielsboiardi <ariels.boiardi@gmail.com>
2019-12-20 09:21:53 +0100

    Update conv_plot.m

--- Simulations/conv_plot.m
+++ Simulations/conv_plot.m
@@ -1,43 +1,3 @@
-clear all; close all; clc
-% addpath(genpath('.'))
-
-%% Dati del problema
-Problema_bd_layer_dx;
-% Problema_bd_layer_sx;
-% Problema_int_layer;
-% Problema_sol_oscillante;
-% Problema_sol_Heaviside_regolarizzata;
-
-
-% Costruzione del problema
-probdata=problem_data_set(Omega, b, m, u0, u1, f, uex);
-t=probdata.Omega(1):0.001:probdata.Omega(2);
-
-%% Costruzione dello spazio di prima approssimazione
-deg=3;
-dofs=40;
-dim=dofs+2; % dimensione dello spazio di approssimazione
-Xi=linspace(probdata.Omega(1), probdata.Omega(2), dim-deg+1); % nodi uniformi
-space=Bspline_space(deg,Xi); % spazio di approssimazione
-% Costruzione dello spazio gerarchico a partire da quello appena costruito
-hspace=HBspline_space(space);
-
-%% Paramtri di risoluzione
-solver_set=HBspline_solver_set;
-solver_set.maxDoF=1000;
-solver_set.maxIter=7;
-solver_set.minPercImpr=0;
-solver_set.minPercIterImpr=0;
-
-solver_set.maxRes=1e-2;
-solver_set.maxRelResLoc=1;
-
-solver_set.Marker='Dor'; solver_set.theta=0.5;
-solver_set.PreMark=true; solver_set.PreMarkPerc=3;
-
-solver_set.FastLocRes=true;
-solver_set.VerboseMode=true;
-
 %% Covnergence plot
 max_iter=solver_set.maxIter;
 for repeat=1:max_iter
@@ -73,14 +33,13 @@ for repeat=1:max_iter
     %% Risolutore UNIFORME con stesi DoF
     Xi=linspace(probdata.Omega(1), probdata.Omega(2), dim-deg+1); % nodi uniformi
     space=Bspline_space(deg,Xi); % spazio di approssimazione
-    [~,uh_unif]=Bspline_solver(probdata,space);
-    uhfn_unif=@(t) space.Bspline_appr(uh_unif,t);
+    [uhfn_unif,uh_unif,A]=Bspline_solver(probdata,space);
     UnifErr(repeat)=L2error(uhfn_unif,probdata.uex,probdata.Omega);
     
     DOFS(repeat)=dim-2;
 end
 
-
+figure
 plot(DOFS, AdaptErr, 'k-o')
 hold on 
 plot(DOFS, UnifErr, 'k-diamond')
